794cc4f920deddcbd34be97eedd4aea868176cb9
fwmakc <fwmakcpro@yandex.ru>
2025-03-05 00:47:39 +0300

    Changes to be committed: modified:   src/files/files.interface.ts modified:   src/mail/interface/attachments.mail.interface.ts

--- src/files/files.interface.ts
+++ src/files/files.interface.ts
@@ -1,13 +1,20 @@
 export class FilesInterface {
   buffer?: Buffer;
+
   error?: string;
+
   mimetype: string;
+
   originalname: string;
+
   size?: number;
+
   timestamp?: Date;
+
   url?: string;
 
   width?: number;
+
   height?: number;
 
   constructor(file: Express.Multer.File | FilesInterface) {
--- src/mail/interface/attachments.mail.interface.ts
+++ src/mail/interface/attachments.mail.interface.ts
@@ -1,6 +1,9 @@
 export class AttachmentsMailInterface {
   filename: string;
+
   content?: string;
+
   encoding?: string;
+
   contentType?: string;
 }
